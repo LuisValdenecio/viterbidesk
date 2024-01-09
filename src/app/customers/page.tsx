394e6182637fb3366d1021ef607661fdee427b3a
Luis Tchitue <ls04af@gmail.com>
2024-01-09 09:13:13 +0100

    sets up the layout for the component route and installs some config

---
+++ src/app/customers/page.tsx
@@ -0,0 +1,12 @@
+import { Customers } from "@/components/Customers";
+import { HeroPattern } from "@/components/HeroPattern";
+import Pagination from "@/components/Pagination";
+
+export default function Page() {
+    return <>
+        <Customers />
+        <div className="mt-10">
+            <Pagination />
+        </div>
+    </>;
+}
